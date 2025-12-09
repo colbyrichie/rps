b4cace09e3d55043e9c8e63fe076aaa9591eb785
Colby Richie <239061815+colbyrichie@users.noreply.github.com>
2025-12-08 20:59:26 -0600

    Delete old css, add html and working javascript

--- javascript.js
+++ javascript.js
@@ -16,31 +16,49 @@ function getComputerChoice() {
     };
 };
 
+const result = document.createElement("div");
+
 function playRound(humanChoice, computerChoice) {
+
+
     if (humanChoice === computerChoice) {
         console.log("Its a tie!");
+        result.textContent = "Its a tie!";
     } else if (humanChoice === "rock" && computerChoice === "paper") {
         console.log("You lose! Paper beats Rock");
         computerScore += 1;
+        result.textContent = "You lose! Paper beats Rock";
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
         console.log("You lose! Scissors beats Paper");
         computerScore += 1;
+        result.textContent = "You lose! Scissors beats Paper";
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
         console.log("You lose! Rock beats Scissors");
         computerScore += 1;
+        result.textContent = "You lose! Rock beats Scissors";
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
         console.log("You win! Rock beats Scissors");
         humanScore += 1;
+        result.textContent = "You win! Rock beats Scissors";
     } else if (humanChoice === "paper" && computerChoice === "rock") {
         console.log("You win! Paper beats Rock");
         humanScore += 1;
+        result.textContent = "You win! Paper beats Rock";
     } else {
         console.log("You win! Scissors beats Paper")
         humanScore += 1;
+        result.textContent = "You win! Scissors beats Paper";
     };
-    console.log("Your score: " + humanScore);
-    console.log("The computers score: " + computerScore);
-    console.log(" ");
+    //console.log("Your score: " + humanScore);
+    //console.log("The computers score: " + computerScore);
+    //console.log(" ");
+
+    const theResultsDiv = document.querySelector(".theResults");
+    theResultsDiv.appendChild(result);
+
+    humanScoreDiv.textContent = "Your score: " + humanScore;
+    computerScoreDiv.textContent = "The computers score: " + computerScore;
+
 };
 
 
@@ -58,3 +76,6 @@ buttons.forEach((button) => {
         playRound(btntext, getComputerChoice());
     });
 });
+
+const humanScoreDiv = document.querySelector(".humanScore");
+const computerScoreDiv = document.querySelector(".computerScore");
